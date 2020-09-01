83dfb673d188bef7ef73536025e025ba2afe21c1
Guillaume Malecha <guillaume.malecha@gmail.com>
2020-09-01 20:32:21 +0200

    update

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Cocktail;
+use App\Entity\Categories;
+use App\Entity\Ingredients;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        //$faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 1; $i <= 20; $i++) {
+            $cocktail = new Cocktail();
+            $cocktail->setNom('cocktail '.$i);
+            $cocktail->setDescription('Description du cocktail');
+            $cocktail->setPrix(mt_rand(5, 15));
+            $cocktail->setOrigine('Origine du cocktail');
+            $cocktail->setImageUrl('https://loremflickr.com/640/360');
+            $manager->persist($cocktail);
+        }
+
+        for ($j = 1; $j <= 6; $j++) {
+            $categorie = new Categories();
+            $categorie->setNom('Catégorie n° '.$j);
+            $categorie->setDescription('Description de la catégorie');
+            $manager->persist($categorie);
+        }
+
+        for ($k = 1; $k <= 60; $k++) {
+            $ingredient = new Ingredients();
+            $ingredient->setNom('Ingrédient '.$k);
+            $ingredient->setDescription('Description de cet ingrédient');
+            $manager->persist($ingredient);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Cocktail.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CocktailRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CocktailRepository::class)
+ */
+class Cocktail
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="string", length=45, nullable=true)
+     */
+    private $origine;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image_url;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(?int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getOrigine(): ?string
+    {
+        return $this->origine;
+    }
+
+    public function setOrigine(?string $origine): self
+    {
+        $this->origine = $origine;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->image_url;
+    }
+
+    public function setImageUrl(string $image_url): self
+    {
+        $this->image_url = $image_url;
+
+        return $this;
+    }
+}
