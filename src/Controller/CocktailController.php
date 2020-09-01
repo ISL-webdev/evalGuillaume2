60f64aef5a1310326b1b442f32c54903d72bf6fb
Guillaume Malecha <guillaume.malecha@gmail.com>
2020-09-01 21:19:10 +0200

    update

--- src/Controller/CocktailController.php
+++ src/Controller/CocktailController.php
@@ -3,7 +3,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Cocktail;
+use App\Entity\Ingredients;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,14 +18,34 @@ class CocktailController extends AbstractController
      */
     public function homepage()
     {
-        return $this->render('home.html.twig', ['title'=>'Bienvenue au Storehouse']);
+        $cocktails = $this->getDoctrine()->getRepository(Cocktail::class)->findAll();
+        return $this->render('home.html.twig', ['title'=>'Bienvenue au Storehouse', 'cocktails'=> $cocktails]);
+    }
+
+    /**
+     * @Route("/cocktails/list")
+     */
+    public function allcocktails()
+    {
+        $cocktails = $this->getDoctrine()->getRepository(Cocktail::class)->findAll();
+        return $this->render('cocktails/cocktails.html.twig', ['title'=>'Bienvenue au Storehouse', 'cocktails'=> $cocktails]);
     }
 
     /**
      * @Route("/cocktail/create")
      */
-    public function create()
+    public function create(Request $request, ObjectManager $manager)
     {
+        $article = new Article();
         return $this->render('cocktails/create.html.twig', ['title'=>'Création de votre cocktail', /*'cocktail' => $cocktail*/]);
+    }
+
+    /**
+     * @Route("/ingredients/list")
+     */
+    public function ingredients()
+    {
+        $ingredients = $this->getDoctrine()->getRepository(Ingredients::class)->findAll();
+        return $this->render('ingredients.html.twig', ['title'=>'Liste des ingrédients', 'ingredients'=> $ingredients]);
     }
 }
